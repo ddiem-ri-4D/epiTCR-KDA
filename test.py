0737b168656f0dce8bb182792a7cdb745d1d0d54
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2023-12-15 11:26:03 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 import random
 import glob
 import itertools
@@ -8,23 +6,25 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import modules.model as Model
-from imblearn.under_sampling import RandomUnderSampler
-
-import sklearn.metrics as metrics
-from keras.models import load_model
-
-from sklearn.model_selection import KFold
-from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import confusion_matrix,accuracy_score,precision_score,recall_score,roc_auc_score,classification_report, roc_curve, auc,f1_score
-from sklearn.metrics import classification_report
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
 import time
 import functools
+from imblearn.under_sampling import RandomUnderSampler
+
+from keras.models import load_model
+import sklearn.metrics as metrics
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix,accuracy_score,precision_score,recall_score,roc_auc_score,classification_report, roc_curve, auc,f1_score
+from sklearn.metrics import classification_report
 
 import modules.architectures as KD
 import modules.processor as Processor
@@ -39,33 +39,24 @@ parser = ArgumentParser(description="Specifying Input Parameters")
 parser.add_argument("-te", "--testfile", help="Specify the full path of the file with TCR sequences")
 parser.add_argument("-sm", "--savedmodel", help="Specify saved model file")
 parser.add_argument("-o", "--outfile", default=sys.stdout, help="Specify output file")
-# parser.add_argument("-sm", "--savemodel", help="Specify the full path of the file with save model")
 
 args = parser.parse_args()
 
 # print('Loading and encoding the dataset..')
 print("###---LOADING DATA")
 
-DATA_TEST = pd.read_csv(args.testfile)
+DATA_TEST = pd.read_parquet(args.testfile)
 DATA_TEST = DATA_TEST[["CDR3b", "epitope", "binder"]]
 
-# DATA_TEST = pd.read_csv("./DATA_FINAL/DATA_TEST_v2.csv")
-
-###--DATA_REPRESENTATION
 print("###---DATA REPRESENTATION")
 
 X_TEST, y_TEST = Processor.DATA_REPRESENTATION(DATA_TEST),  DATA_TEST[["binder"]]
 X_TEST_cv, y_TEST_cv = Processor.cv_data_kd(X_TEST), np.squeeze(np.array(y_TEST))
 
-
-###--TRAINING
-
-
 # student_scratch.save("model.h5")
 # student_scratch.save(args.savedmodel)
 student_scratch = load_model(args.savedmodel)
 
-###---Evaluation
 print("###---EVALUATION")
 
 predicted_probabilities = student_scratch.predict(X_TEST_cv)
@@ -77,8 +68,4 @@ data_pred = pd.concat([DATA_TEST, df_label], axis=1)
 
 print("###---SAVE DATA")
 
-data_pred.to_csv(args.outfile, index=False)
-
-# lst_unseen_result, l_lst_unseen = Processor.fn_lst_unseen(DATA_TRAIN, DATA_TEST)
-# Model.modeling_kd(data_pred,lst_unseen_result)
-
+data_pred.to_parquet(args.outfile)
