4efc859ac86093dac741350f5e40d113063bdab2
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-05-24 00:30:26 +0700

    Update checkHavePDB.py

--- utils/checkHavePDB.py
+++ utils/checkHavePDB.py
@@ -11,10 +11,10 @@ def read2seq(LST_SEQ, thu_muc):
     FINAL_TSV = [cdr3 for cdr3 in LST_SEQ if cdr3 not in danh_sach_tep_tsv]
     return FINAL_TSV
 
-LST_SEQ = read2seq(df_CDR3b, thu_muc_pep)
+LST_SEQ = read2seq(DF.CDR3b.unique(), thu_muc_pep)
 
 
-FILE = "./LST_SEQ_REMAIN_COVID.fasta"
+FILE = "./LST_FORRUN.fasta"
 DATA = LST_SEQ
 
 with open(FILE, 'w') as fp:
