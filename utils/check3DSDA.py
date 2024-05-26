e5a349fda691a63d2d179b1b1626bd3e1ec99956
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-05-26 22:59:09 +0700

    Add files via upload

---
+++ utils/check3DSDA.py
@@ -0,0 +1,24 @@
+#Check have in PDB
+
+import os
+thu_muc_pep =  './datasets/DA_TSVFiles'
+
+def read2seq(LST_SEQ, thu_muc):
+    danh_sach_tep_tsv = []
+    for tep in os.listdir(thu_muc):
+        if tep.endswith(".tsv"):
+            danh_sach_tep_tsv.append(tep[:-4])
+    FINAL_TSV = [cdr3 for cdr3 in LST_SEQ if cdr3 not in danh_sach_tep_tsv]
+    return FINAL_TSV
+
+LST_SEQ = read2seq(DF.CDR3b.unique(), thu_muc_pep)
+
+
+FILE = "./LST_FORRUN.fasta"
+DATA = LST_SEQ
+
+with open(FILE, 'w') as fp:
+    for item in DATA:
+        fp.write(">%s\n" % item)
+        fp.write("%s\n" % item)
+    print('Done')
