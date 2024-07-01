5d8013e05c86f8094130ac20f442c9cb08e7232a
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-07-01 23:59:01 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,11 @@ import sys
 import pandas as pd
 import numpy as np
 import modules.model as Model
-
 from keras.models import load_model
 import modules.processor as Processor
 import modules.model as Model
 
 warnings.filterwarnings('ignore')
-
 from argparse import ArgumentParser
 
 #Args parse
@@ -25,7 +23,6 @@ args = parser.parse_args()
 
 # print('Loading and encoding the dataset..')
 print("###---LOADING DATA")
-
 DATA_TEST = pd.read_parquet(args.testfile)
 DATA_TEST = pd.read_parquet(args.testfile)
 
@@ -33,9 +30,7 @@ DATA_TEST = Processor.check_length_tcr(DATA_TEST)
 DATA_TEST = Processor.check_length_epi(DATA_TEST)
 DATA_TEST = DATA_TEST.reset_index(drop=True)
 
-###--DATA_REPRESENTATION
 print("###---DATA REPRESENTATION")
-
 X_TEST = Processor.DATA_REPRESENTATION(DATA_TEST)
 X_TEST_cv = Processor.cv_data_kd(X_TEST)
 
@@ -44,7 +39,6 @@ student_scratch = load_model(args.savedmodel)
 
 ###---Evaluation
 print("###---EVALUATION")
-
 predicted_probabilities = student_scratch.predict(X_TEST_cv)
 predicted_labels = predicted_probabilities.argmax(axis=1)
 predicted_labels = (predicted_probabilities >= 0.5).astype(int)
@@ -53,5 +47,4 @@ df_label = pd.DataFrame(zip(predicted_probabilities.squeeze(), predicted_labels.
 data_pred = pd.concat([DATA_TEST, df_label], axis=1)
 
 print("###---SAVE DATA")
-
 data_pred.to_parquet(args.outfile)
