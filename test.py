2471cc1bfe1e8e7e78287e3fe036c3c0637397cf
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-07-01 23:59:57 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,13 @@ import pandas as pd
 import warnings
 import os
 import sys
-import pandas as pd
-import numpy as np
-import modules.model as Model
 from keras.models import load_model
+from argparse import ArgumentParser
 import modules.processor as Processor
-import modules.model as Model
 
 warnings.filterwarnings('ignore')
-from argparse import ArgumentParser
 
-#Args parse
+# Args parse
 parser = ArgumentParser(description="Specifying Input Parameters")
 parser.add_argument("-te", "--testfile", help="Specify the full path of the file with TCR sequences")
 parser.add_argument("-sm", "--savedmodel", help="Specify saved model file")
@@ -21,9 +17,8 @@ parser.add_argument("-o", "--outfile", default=sys.stdout, help="Specify output 
 
 args = parser.parse_args()
 
-# print('Loading and encoding the dataset..')
+# Load and encode the dataset
 print("###---LOADING DATA")
-DATA_TEST = pd.read_parquet(args.testfile)
 DATA_TEST = pd.read_parquet(args.testfile)
 
 DATA_TEST = Processor.check_length_tcr(DATA_TEST)
@@ -34,17 +29,18 @@ print("###---DATA REPRESENTATION")
 X_TEST = Processor.DATA_REPRESENTATION(DATA_TEST)
 X_TEST_cv = Processor.cv_data_kd(X_TEST)
 
-###--LOAD MODEL PRETRAINING
+# Load pre-trained model
+print("###---LOAD MODEL PRETRAINING")
 student_scratch = load_model(args.savedmodel)
 
-###---Evaluation
+# Evaluation
 print("###---EVALUATION")
 predicted_probabilities = student_scratch.predict(X_TEST_cv)
-predicted_labels = predicted_probabilities.argmax(axis=1)
 predicted_labels = (predicted_probabilities >= 0.5).astype(int)
 
-df_label = pd.DataFrame(zip(predicted_probabilities.squeeze(), predicted_labels.squeeze()), columns=['proba_pred', 'binder_pred'])
+df_label = pd.DataFrame({'proba_pred': predicted_probabilities.squeeze(), 'binder_pred': predicted_labels.squeeze()})
 data_pred = pd.concat([DATA_TEST, df_label], axis=1)
 
+# Save the predictions
 print("###---SAVE DATA")
 data_pred.to_parquet(args.outfile)
