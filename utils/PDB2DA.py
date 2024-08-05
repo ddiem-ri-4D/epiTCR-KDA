f30d96a01b96fea57840dd750ada2d571ba9edef
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-08-06 00:27:42 +0700

    Update PDB2DA.py

--- utils/PDB2DA.py
+++ utils/PDB2DA.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
-import random
-import glob
-import itertools
-from biopandas.pdb import PandasPdb
 import os
-import Bio.PDB
+from Bio.PDB import PDBParser, CaPPBuilder
 from math import degrees
 
 def ramachandran_type(phi, psi):
@@ -33,38 +29,33 @@ def ramachandran_type(phi, psi):
     else:
         return "Unknown"
 
-folder_path = "./datasets/DA_TSVFiles"
+pdb_folder_path = "./datasets/DA_TSVFiles"
+output_folder_path = "./datasets/3DS_PDBFiles"
 file_extension = ".pdb"
 
-# Lấy danh sách các file trong thư mục FINAL_PEP/
-file_list = [filename for filename in os.listdir(folder_path) if filename.endswith(file_extension)]
+# List of PDB files in the folder
+pdb_file_list = [filename for filename in os.listdir(pdb_folder_path) if filename.endswith(file_extension)]
 
-for pdb_file in file_list:
+for pdb_file in pdb_file_list:
     pdb_code = os.path.splitext(pdb_file)[0]
-    link_code = "./datasets/3DS_PDBFiles" + pdb_code
+    output_file_path = os.path.join(output_folder_path, f"{pdb_code}.tsv")
     
-    print("About to load Bio.PDB and the PDB file...")
-    structure = Bio.PDB.PDBParser().get_structure(pdb_code, os.path.join(folder_path, pdb_file))
-    print("Done")
+    print("Loading PDB file using Bio.PDB...")
+    structure = PDBParser().get_structure(pdb_code, os.path.join(pdb_folder_path, pdb_file))
+    print("Done loading PDB file.")
 
-    print("About to save angles to file...")
-    output_file = open("%s.tsv" % link_code, "w")
-    print(output_file)
-    for model in structure:
-        for chain in model:
-            print("Chain %s" % str(chain.id))
-            polypeptides = Bio.PDB.CaPPBuilder().build_peptides(chain)
-            for poly_index, poly in enumerate(polypeptides):
-                phi_psi = poly.get_phi_psi_list()
-                for res_index, residue in enumerate(poly):
-                    phi, psi = phi_psi[res_index]
-                    if phi is not None and psi is not None:
-                        # Don't write output when missing an angle
-                        phi_angle = degrees(phi)
-                        psi_angle = degrees(psi)
-                        output_file.write("%s:Chain%s:%s%i\t%f\t%f\t%s\n" \
-                            % (pdb_code, str(chain.id), residue.resname,
-                               residue.id[1], phi_angle, psi_angle,
-                               ramachandran_type(phi_angle, psi_angle)))
-    output_file.close()
-    print("Done")
+    print("Saving angles to file...")
+    with open(output_file_path, "w") as output_file:
+        for model in structure:
+            for chain in model:
+                print(f"Processing Chain {chain.id}...")
+                polypeptides = CaPPBuilder().build_peptides(chain)
+                for poly_index, poly in enumerate(polypeptides):
+                    phi_psi = poly.get_phi_psi_list()
+                    for res_index, residue in enumerate(poly):
+                        phi, psi = phi_psi[res_index]
+                        if phi is not None and psi is not None:
+                            phi_angle = degrees(phi)
+                            psi_angle = degrees(psi)
+                            output_file.write(f"{pdb_code}:Chain{chain.id}:{residue.resname}{residue.id[1]}\t{phi_angle}\t{psi_angle}\t{ramachandran_type(phi_angle, psi_angle)}\n")
+    print("Done saving angles.")
