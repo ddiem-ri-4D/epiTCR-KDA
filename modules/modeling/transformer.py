a5520eb50db5453e71dd556a610ab96e2406c10c
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-08-09 00:07:03 +0700

    Create transformer.py

---
+++ modules/modeling/transformer.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.metrics import roc_auc_score
+from argparse import ArgumentParser
+import modules.processor as processor
+import warnings
+import sys
+
+warnings.filterwarnings('ignore')
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Normalization and Attention
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(key_dim=head_size, num_heads=num_heads, dropout=dropout)(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res
+
+def build_model(input_shape, head_size, num_heads, ff_dim, num_transformer_blocks, mlp_units, dropout=0, mlp_dropout=0):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    return keras.Model(inputs, outputs)
+
+def main():
+    # Argument parsing
+    parser = ArgumentParser(description="Specifying Input Parameters")
+    parser.add_argument("-tr", "--trainfile", help="Specify the full path of the training file with TCR sequences")
+    parser.add_argument("-te", "--testfile", help="Specify the full path of the file with TCR sequences")
+    parser.add_argument("-sm", "--savemodel", help="Specify save model file")
+    parser.add_argument("-otest", "--outfiletest", default=sys.stdout, help="Specify output file")
+
+    args = parser.parse_args()
+
+    print("###---LOADING DATA")
+    train_data = pd.read_parquet(args.trainfile)
+    test_data = pd.read_parquet(args.testfile)
+    train_data = train_data.reset_index(drop=True)
+    test_data = test_data.reset_index(drop=True)
+
+    train_data = train_data[["CDR3b", "epitope", "binder"]]
+    test_data = test_data[["CDR3b", "epitope", "binder"]]
+
+    print("###---DATA REPRESENTATION")
+    tcr_epitope_split = processor.data_representation(train_data)
+    full_train_data = pd.concat([train_data, tcr_epitope_split], axis=1)
+    X_train, y_train = processor.downsample_data(full_train_data)
+
+    X_test, y_test = processor.data_representation(test_data), test_data[["binder"]]
+
+    X_train_cv, y_train_cv = processor.prepare_cv_data(X_train), np.squeeze(np.array(y_train))
+    X_test_cv, y_test_cv = processor.prepare_cv_data(X_test), np.squeeze(np.array(y_test))
+
+    print("###---TRAINING")
+
+    input_shape = (17, 4)
+    model = build_model(
+        input_shape,
+        head_size=256,
+        num_heads=4,
+        ff_dim=4,
+        num_transformer_blocks=4,
+        mlp_units=[128],
+        mlp_dropout=0.4,
+        dropout=0.25,
+    )
+
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=keras.optimizers.Adam(learning_rate=1e-4),
+        metrics=["accuracy"],
+    )
+
+    model.summary()
+
+    model.fit(X_train_cv, y_train_cv, validation_split=0.2, epochs=5, batch_size=64)
+
+    loss, accuracy = model.evaluate(X_test_cv, y_test_cv)
+    auc = roc_auc_score(y_test_cv, model.predict(X_test_cv))
+
+    print(f"Transformer Accuracy: {accuracy}")
+    print(f"Transformer AUC: {auc}")
+
+    model.save(args.savemodel)
+
+    print("###---EVALUATION-TEST")
+
+    predicted_probabilities = model.predict(X_test_cv)
+    predicted_labels = (predicted_probabilities >= 0.5).astype(int)
+
+    df_label = pd.DataFrame({
+        'proba_pred': predicted_probabilities.squeeze(),
+        'binder_pred': predicted_labels.squeeze()
+    })
+    data_pred = pd.concat([test_data, df_label], axis=1)
+
+    print("###---SAVE DATA TEST")
+    data_pred.to_parquet(args.outfiletest)
+
+if __name__ == "__main__":
+    main()
